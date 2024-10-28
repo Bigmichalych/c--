d1a47900f2f990d57f7f7fcd7fe6e57103e1cbb7
Bigmichalych <bigmichalych@gmail.com>
2024-10-28 14:38:42 +0300

    Add files via upload

---
+++ dzbz.cpp
@@ -0,0 +1,24 @@
+﻿#define _USE_MATH_DEFINES
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+    float x, y;
+    std::cout << "Input x and y values: ";
+    std::cin >> x >> y;
+
+    double R = cos(x / y);
+    double S = pow(2, -3 * x) + pow(M_PI * y, 1 / 3);
+    double C = std::max(R, S);
+
+
+    std::cout << "R = " << R << "\n";
+    std::cout << "S = " << S << "\n";
+    std::cout << "C = " << C << "\n";
+
+    std::cout << "Press Enter to end...";
+    system("pause");
+
+    return 0;
+}
